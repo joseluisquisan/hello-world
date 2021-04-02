b1cdda65ea70697553de56eb73ddc7557e642d5a
Jose Luis Quintero Sanchez <joseluisquisan@gmail.com>
2021-04-02 19:02:20 -0300

    agregando monedas y menu

--- helloworld.py
+++ helloworld.py
@@ -3,12 +3,16 @@
 def run():
     menu = """
     Conversor de Monedas
-    1 - Cambiar monedad Chilenas
-    2 - Cam
+    1 - Cambiar monedas Chilenas
+    2 - Cambiar monedas Argentinas
     """
-
     print(menu)
+    opcion = input("Opcion Elegida: ")
 
+    if opcion == 1:
+        print("Son Chilenos")
+    else:
+        print("son argentinos")
 
 if __name__ == '__main__':
     run()
