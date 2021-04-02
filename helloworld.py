6d92bf03c1181e7c6411304f4ce61fd41933846e
Jose Luis Quintero Sanchez <joseluisquisan@gmail.com>
2021-04-02 18:57:43 -0300

    Agregando el menu

--- helloworld.py
+++ helloworld.py
@@ -1,7 +1,13 @@
 #!/usr/bin/python3
 
 def run():
-    print("Hola mundo3!")
+    menu = """
+    Conversor de Monedas
+    1 - Cambiar monedad Chilenas
+    2 - Cam
+    """
+
+    print(menu)
 
 
 if __name__ == '__main__':
