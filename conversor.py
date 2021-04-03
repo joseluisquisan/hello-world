b51ef9d147a15dc3a7ee456da874e4df675b3d8f
Jose Luis Quintero <joseluisquisan@gmail.com>
2021-04-03 08:46:29 -0300

    actualizando con los paises

--- conversor.py
+++ conversor.py
@@ -1,20 +1,32 @@
 #!/usr/bin/python3
 
+def conversor(pesos, dolar):
+    cambio = round((pesos / dolar), 2)
+    print("Tienes $", cambio, "dolares")
+
 def run():
     menu = """
     Conversor de Monedas
     1 - Cambiar monedas Chilenas a Dolares
-    2 - Cambiar monedas Argentinas
+    2 - Cambiar monedas Argentinas a Dolares
+    3 - Cambiar monedas Venezolanas a Dolares
     """
     print(menu)
     opcion = int(input("Opcion Elegida: "))
     if opcion == 1:
         pesos = int(input("¿Cuantos pesos Chilenos tienes?: "))
-        dolar = 716
-        cambio = pesos / dolar
-        print("Tienes", cambio, "dolares")
+        dolar = 720
+        conversor(pesos, dolar)
+    elif opcion == 2:
+        pesos = int(input("¿Cuantos pesos Argentinos tienes?: "))
+        dolar = 7000
+        conversor(pesos, dolar)
+    elif opcion == 3:
+        pesos = int(input("¿Cuantos Bolivares tienes?: "))
+        dolar = 2000
+        conversor(pesos, dolar)
     else:
-        print("error")
+        print("===== Error, Favor elegir opcion correcta")
 
 if __name__ == '__main__':
     run()
--- testapi.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/python3
-import requests
-
-obtener = requests.get('https://icanhazdadjoke.com')
-print("========================")
-print(obtener.status_code)
-print("========================")
-print(obtener)
