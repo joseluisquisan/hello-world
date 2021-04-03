c8057bd0b3e4694927dfb68aa7cc849d33553ea5
Jose Luis Quintero <joseluisquisan@gmail.com>
2021-04-03 15:55:20 -0300

    reduciendo lineas de codigo en Conversor

--- conversor.py
+++ conversor.py
@@ -1,8 +1,18 @@
 #!/usr/bin/python3
 
-def conversor(pesos, dolar):
+# Funciones para Modificar el String(Nombre)
+# nombre = nombre.capitalize()  # Para poner la primera letra en Mayusculas
+# nombre = nombre.upper() # Para poner todo en Mayusc
+# nombre = nombre.lower() # Para poner todo en Minusc
+# nombre = nombre.strip() # Para quitar espacios en Blanco
+# nombre = nombre.replace('a' , 'o') # Reemplaca 'a' por 'o'
+# nombre = nombre.strip() # Para quitar espacios en Blanco
+# len(nombre) # Para saber cuantos caracteres tiene
+
+def conversor(moneda, dolar):
+    pesos = int(input("¿Cuantos pesos " + moneda + " tienes?: "))
     cambio = round((pesos / dolar), 2)
-    print("Tienes $", cambio, "dolares")
+    print("Tienes $", cambio , moneda, sep=" ")
 
 def run():
     menu = """
@@ -14,17 +24,14 @@ def run():
     print(menu)
     opcion = int(input("Opcion Elegida: "))
     if opcion == 1:
-        pesos = int(input("¿Cuantos pesos Chilenos tienes?: "))
         dolar = 720
-        conversor(pesos, dolar)
+        conversor("Pesos Chilenos", dolar)
     elif opcion == 2:
-        pesos = int(input("¿Cuantos pesos Argentinos tienes?: "))
-        dolar = 7000
-        conversor(pesos, dolar)
+        dolar = 720
+        conversor("Pesos Argentinos", dolar)
     elif opcion == 3:
-        pesos = int(input("¿Cuantos Bolivares tienes?: "))
         dolar = 2000
-        conversor(pesos, dolar)
+        conversor("Bolivares", dolar)
     else:
         print("===== Error, Favor elegir opcion correcta")
         return run()
