bc3d9f339490d8bb5808ddc6b0c33476edf3b6e4
Jose Luis Quintero <joseluisquisan@gmail.com>
2021-04-04 11:28:19 -0400

    arreglando bucles

--- bucles.py
+++ bucles.py
@@ -1,10 +1,9 @@
 #!/usr/bin/python3
 
-
 def run():
-    limite = int(input('Hasta donde quiere que elevemos? : 5'))
-    contador = 0
-    while contador < limite:
+    limite = int(input('Hasta donde quiere que elevemos? : '))
+    elevacion = 0
+    while elevacion < limite:
         print('La potencia de 2 elevado a la ', elevacion, 'es igual', 2**elevacion)
         elevacion = elevacion + 1
 
